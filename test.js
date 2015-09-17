1706611a012da75f8802de453efd91ce62894363
AGhost-7 <jonathan.boudreau.92@gmail.com>
2015-09-17 19:40:37 -0400

    Add one more test, and enable travis

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 var mkFn = require('./index.js');
 var assert = require('assert');
 
-var fn0 = function() {};
+var fn0 = function() { return 1; };
 
 var fn4 = mkFn(4, fn0);
-assert.equal(fn4.length, 4, 'generates a function with given lenght');
+assert.equal(fn4.length, 4, 'generates a function with given length');
+
+assert.equal(fn0(), 1, 'it returns the result of the wrapped function');
 
 var fn5 = mkFn(5, undefined);
 assert.equal(fn5.length, 5, 'does not fail even if function argument is invalid');
+
+console.log('all tests passed');
